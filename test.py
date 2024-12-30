76a79956a7c289161419437ae48a5f4e57f30504
joaopauloml4 <joao.lopes@discente.ufcat.edu.br>
2024-12-30 13:38:10 -0300

    python 0.6

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 #pergunta para usuario e anexo de valor.
 idade = int
-name = input("Qual seu nome? " ,end=(" "))
+name = input("Qual seu nome? " )
 idade = input("Quantos anos voce tem? ")
 cidade = input("Em qual cidade voce mora? ")
 """
@@ -8,4 +8,5 @@ ISSO TAMBEM E UM COMENTARIO
 """
 #saida final
 #sep= o que vai acontecer entre os parametros.
+#end= o que vai acontecer ao final.
 print ("ola",name,"vimos que tens",idade,"anos e mora na cidade de",cidade, sep=(" "), end=(" "))
