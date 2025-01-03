54a7eab031e5da775a6b34861206895dafba5195
joaopauloml4 <joao.lopes@discente.ufcat.edu.br>
2025-01-03 14:07:31 -0300

    aula int

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 #pergunta para usuario e anexo de valor.
 name = input("Qual seu nome? " )
-idade = input("Quantos anos voce tem? ")
+idade = input("Quantos anos voce tem? ").strip() #retira os espaços, posso usar todos assim.
 cidade = input("Em qual cidade voce mora? ")
 
 #retirar os espaços da str
 name = name.strip()
-idade = idade.strip()
-cidade = cidade.strip()
+#começar com maiuscula, primeirs
+name = name.title()
+#retirar espaços e a primeira letra da entrada é maiuscula
+cidade = cidade.strip().capitalize()
+
+first, last = name.split(" ")
 """
 ISSO TAMBEM E UM COMENTARIO
 """
@@ -15,4 +19,4 @@ ISSO TAMBEM E UM COMENTARIO
 #end= o que vai acontecer ao final.
 print ("ola",name,"vimos que tens",idade,'anos e mora na cidade de',cidade, sep=(" "), end=("\n"))
 #outro meio de colocar variaveis, é necessario colocar {} e "f" no inicio da linha. (não se pode usar sep and end.)
-print (f"ola {name} vimos que tens {idade} anos e mora na cidade de {cidade}")
+print (f"ola {first} vimos que tens {idade} anos e mora na cidade de {cidade}")
